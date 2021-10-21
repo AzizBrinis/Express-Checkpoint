4bad377c4c383c34598e7f1abbeeed557fc3424d
Aziz <brinisaziz@yahoo.fr>
2021-10-21 13:20:01 +0100

    first commit

---
+++ Desktop/Nouveau dossier/doh/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+    return(<div><h1>Hello</h1><h2>Dohdoh</h2><h3>Salut</h3></div>)
+};
+
+export default App;
---
+++ Desktop/hello/hello/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <div>
+        <h1>Hello</h1>
+        <h2>World</h2>
+    </div>,
+    document.getElementById("root"));
